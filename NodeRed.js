3f58f228c8fdebdd5cf47719c98310b91c83393e
Sebastian Schöneich <107889589+NeroAiur@users.noreply.github.com>
2022-07-18 08:49:30 +0200

    corrected typo

--- NodeRed.js
+++ NodeRed.js
@@ -4,4 +4,4 @@ const lightLv = payload.ANALOG.Illuminance
 node.warn(lightLv)
 
 const LightSwitch = lightLv > 20 ? {payload: 'ON', topic: 'POWER'} : {payload: 'OFF', topic: 'POWER'}
-return LightSwitc
+return LightSwitch
